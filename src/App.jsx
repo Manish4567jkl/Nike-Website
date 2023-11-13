e7d01aed1d4f279b6abea342d68ff3e427a7a0ab
Manish4567jkl <manishmandal895@gmail.com>
2023-11-13 16:25:50 +0530

    added assets and update configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-
-import './App.css'
-
-function App() {
-
-
+const App = ()=> {
   return (
     <>
-    <h1 className='text-9xl text-emerald-300'>
-      start
-    </h1>
+  <main className='relative'>
+      Nav 
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        Hero 
+      </section>
+      <section className='padding'>
+        PopularProducts 
+      </section>
+      <section className='padding'>
+        SuperQuality 
+      </section>
+      <section className='padding-x py-10'>
+        Services
+      </section>
+      <section className='padding'>
+        SpecialOffer 
+      </section>
+      <section className='bg-pale-blue padding'>
+        CustomerReviews 
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        Subscribe 
+      </section>
+      <section className=' bg-black text-slate-300 padding-x padding-t pb-8'>
+        Footer 
+      </section>
+    </main>
     </>
   )
 }
